37d89592e555af33d63194851a4406649034e270
cyberforce <cyberforce@indonesiaindicator.com>
2024-09-30 10:54:57 +0700

    update

---
+++ ubuntu-22.sh
@@ -0,0 +1,41 @@
+# Set environment to avoid interactive prompts
+export DEBIAN_FRONTEND=noninteractive
+
+# Update package list
+apt update -y
+
+# Disable UFW
+ufw disable
+
+
+
+#NTP Section
+
+# Install NTP without prompts
+apt install ntp -y
+
+# Set the system timezone to UTC manually
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+# Restart NTP to apply changes
+service ntp restart
+
+# Verify the timezone setting
+date
+
+
+#Add Open Files Limit
+bash -c "echo '
+# Ulimit settings for the root user
+root soft     nproc          65535
+root hard     nproc          65535
+root soft     nofile         65535
+root hard     nofile         65535
+' >> /etc/security/limits.conf"
+
+echo "Add ulimit complete"
+
+
+# Update Inotify Watchers
+echo fs.inotify.max_user_watches=5242880 | tee -a /etc/sysctl.conf
+sysctl -p

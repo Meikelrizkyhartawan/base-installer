592b4131b67be88097ec697416238d7bf6f1a30e
cyberforce <cyberforce@indonesiaindicator.com>
2024-09-30 11:02:02 +0700

    update

--- README.sh
+++ README.sh
@@ -1,2 +1,8 @@
 # Introduction
 Tuning OS system settings is crucial for gaining future insights that will assist administrators in determining necessary configurations without the need for frequent updates to accommodate incoming services.
+
+# How to run
+```
+chmod +x ubuntu-22.sh
+sh ubuntu-22.sh
+```
